c83e2e9e85b28036ddde6a793aec2709e60bb05a
Pepe <pepebogi5@gmail.com>
2024-01-19 15:40:34 +0100

    Example readme

---
+++ README.md
@@ -0,0 +1,26 @@
+# Advanced Python
+
+Explain what my repository does
+
+## Table of Contents
+
+- Installation
+- Usage
+- Test
+
+## Installation
+
+    pip install -r requirements.txt
+
+## Usage
+
+    python scripts/repo_first_script.py -i datasets/BooksDatasetClean.csv
+
+Explain the command
+Explain additional arguments
+
+## Test
+
+    pytest
+
+15 tests should pass
--- scripts/simple_functions.py
+++ scripts/simple_functions.py
@@ -1,8 +1,38 @@
 """
 Script to write sum and subtract functions
 """
+import time
+
+# def example_decorator(func):
+#     """
+#     Example decorator
+#     """
+#     def wrapper(*args, **kwargs):
+#         print('Hello')
+#         result = func(*args, **kwargs)
+#         print('Done')
+#         print('World')
+#         return result
+
+#     return wrapper
 
 
+def time_decorator(func):
+    """
+    Example decorator
+    """
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        time.sleep(1)
+        result = func(*args, **kwargs)
+        print(f'The code took: {time.time() - start} seconds')
+        return result
+
+    return wrapper
+
+
+# @example_decorator
+@time_decorator
 def sum_two_nums(x, y):
     """
     Sum two numbers
@@ -10,7 +40,7 @@ def sum_two_nums(x, y):
 
     return x + y
 
-
+@time_decorator
 def sub_two_nums(x, y):
     """
     Subtract two numbers
@@ -20,4 +50,6 @@ def sub_two_nums(x, y):
 
 
 if __name__ == "__main__":
-    pass
+    print(sum_two_nums(2, 3))
+    print(sub_two_nums(2, 3))
+    # pass
